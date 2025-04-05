04d5f9bad378d096345a714fb3fa430c2fa839dd
akxbrand <akxbrand@gmail.com>
2025-04-05 12:01:55 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,23 @@
-// next-sitemap.config.js
+// // next-sitemap.config.js
+// module.exports = {
+//     siteUrl: 'https://akxbrand.com', // your live domain
+//     generateRobotsTxt: true, // generates robots.txt file
+// };
+  /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://akxbrand.com', // your live domain
-    generateRobotsTxt: true, // generates robots.txt file
+  siteUrl: 'https://akxbrand.com',
+  generateRobotsTxt: true,
+  exclude: [
+    '/admin*',
+    '/network-error',
+    '/login',
+    '/register',
+    '/account',
+    '/orders',
+    '/cart',
+    '/checkout',
+    '/admin-login'
+  ],
+  changefreq: 'daily',
+  priority: 0.7,
 };
-  
